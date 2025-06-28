22550d9f6b5df89f57d708d068fc6c65bc5f0fa2
steelx86 <20195teel@gmail.com>
2025-06-27 20:00:04 -0400

    done

--- README.md
+++ README.md
@@ -7,9 +7,3 @@ I use Ocaml and Python to solve the problems ( with some others on occasion )
 
 Sorry If I skipped some of the problems. Some of the ealier ones can just be solved with a list comprehension in python
 
-## personal rules!
----
-
-- I have to use just standard vim
-- No external answers (unless the originals are lost)
-- all answers must be given in under a minute
--- unsolved/problem_37.py
+++ unsolved/problem_37.py
@@ -3,6 +3,37 @@
 from sympy import isprime
 
 
+def is_truncatable(num: int):
+    num_str = str(num)
+    
+    for i in range(len(num_str)):
+        truncated = int(num_str[:len(num_str)-i])
+        if not isprime(truncated):
+            return False
+    
+    for i in range(len(num_str)):
+        truncated = int(num_str[i:])
+        if not isprime(truncated):
+            return False
+        
+    return True
+
+
+def find_truncatable(limit: int) -> list:
+    truncatable_primes = []
+    num = 11 # to speed things up
+    
+    # just adding and checking should work?
+    while len(truncatable_primes) < limit:
+        if isprime(num) and is_truncatable(num):
+            truncatable_primes.append(num)
+        num += 1
+        
+    return truncatable_primes
+
 
 if __name__ == '__main__':
-    pass
+    places = 11
+    answer = sum(find_truncatable(places))
+    print(f'at {places} the answer is {answer}')
+    
